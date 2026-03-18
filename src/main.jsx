0755c9526696edad7068f5996fdb3028d8114207
jithin <jithin@Jithins-MacBook-Air.local>
2026-03-18 12:46:10 +0530

    Restore KSRTC app as main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import ClinicQueueApp from './ClinicQueueApp'
+import KSRTCNextBusApp from './KSRTCNextBusApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ClinicQueueApp />
+        <KSRTCNextBusApp />
     </React.StrictMode>,
 )
